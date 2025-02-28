b5dc11e583b295f12ad35de25e9f01a1451b7a8c
myLaptop <developerforhad24@gmail.com>
2025-02-28 22:33:09 +0600

    Post API to add single user

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -4,16 +4,57 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserApiController extends Controller
 {
+    // This method use for fetch users
     public function showUser($id=NULL){
         if($id == ''){
             $users = User::get();
-            return response()->json(['users' => $users], 200); //Success OK
+            return response()->json(['users' => $users], 200); //Success- OK
         }else{
             $users = User::find($id);
             return response()->json(['users' => $users], 200);
         }
     }
+
+    // This method use for add user
+    public function addUser(Request $request){
+        if($request->isMethod('POST')){
+            $data = $request->all();
+            // return $data;
+
+            $rules = [
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required',
+            ];
+
+            $customMessage = [
+                'name.required' => 'Name is required',
+                'email.required' => 'Email is required',
+                'email.email' => 'Email must be a valid email',
+                'email.unique' => 'Email must be unique',
+                'password.required' => 'Password is required',
+            ];
+
+            $validator = Validator::make($data, $rules, $customMessage);
+            if($validator->fails()){
+                return response()->json($validator->errors(), 422);     // Not Created
+            }
+
+            $user = new User();
+            $user->name = $data['name'];
+            $user->email = $data['email'];
+            $user->password = bcrypt($data['password']);
+
+            $user->save();
+            $message = 'User Successfully Created';
+
+            return response()->json(['message' => $message], 201); // success- CREATED
+        }
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+// Get api for fetch data
 Route::get('/users/{id?}', [UserApiController::class, 'showUser']);
+
+// Post api for add data
+Route::post('/add-user', [UserApiController::class, 'addUser']);
